9e1b98551c5a2f9be3b1f2e98be31cad5496b8b5
hareeshweb2 <hareesh.web2@gmail.com>
2017-11-12 21:02:20 +0530

    primeng added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import {DropdownModule} from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,DropdownModule, FormsModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
